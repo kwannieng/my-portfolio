c3b46b354eca9741f28f48ba04beb0bf9b537935
kwannieng <78864242+kwannieng@users.noreply.github.com>
2023-03-14 20:28:44 +0000

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Gallery from "./pages/Gallery";
